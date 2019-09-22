993622d08e634f2bc0952bb0d2dd64808bff0912
Kyle Mulqueen <kmulqdrums710@gmail.com>
2019-09-22 00:38:06 -0600

    Added lobby controller.

---
+++ controllers/lobbyController.js
@@ -0,0 +1,167 @@
+const User = require("../models/User");
+const Game = require("../models/Game");
+const Lobby = require("../models/Lobby");
+
+module.exports = {
+  getAllLobbies: async (req, res) => {
+    try {
+      const lobbies = await Lobby.find()
+        .populate("host", ["playerInfo", "_id", "name"])
+        .populate("game")
+        .populate("players", ["playerInfo", "_id", "name"]);
+      res.json(lobbies);
+    } catch (error) {
+      res.status(300).json(error);
+    }
+  },
+  getLobbyByID: async (req, res) => {
+    try {
+      const lobby = await Lobby.findById(req.params.lobby_id)
+        .populate("players", ["playerInfo", "_id", "name"])
+        .populate("host", ["playerInfo", "_id", "name"])
+        .populate("game");
+      res.json(lobby);
+    } catch (error) {
+      res.status(300).json(error);
+    }
+  },
+  createLobby: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.user_id)
+        .select("-password")
+        .select("-email");
+
+      const loggedInUser = await User.findById(req.user.id);
+
+      if (user._id.toString() !== loggedInUser.id) {
+        return res.status(401).json({ msg: "You are not authorized" });
+      }
+
+      const { name } = req.body;
+      let lobby = await Lobby.findOne({ name });
+      if (lobby) {
+        return res
+          .status(400)
+          .json({ msg: "Lobby with that name already exists" });
+      }
+
+      const gameSetup = {
+        start: false,
+        gif: "",
+        players: [user],
+        whiteCardPlayers: [],
+        blackCardPlayer: null,
+        whiteCardPicks: [],
+        blackCardPick: null,
+        gameWinner: null
+      };
+      const game = new Game(gameSetup);
+
+      const lobbySetup = {
+        name,
+        host: user,
+        game,
+        players: [user],
+        readyPlayers: []
+      };
+      lobby = new Lobby(lobbySetup);
+
+      await game.save();
+      await lobby.save();
+      res.json(lobby);
+    } catch (error) {
+      res.status(300).json(error);
+    }
+  },
+  joinLobby: async (req, res) => {
+    try {
+      const lobby = await Lobby.findById(req.params.lobby_id)
+        .populate("players", ["playerInfo", "name", "_id"])
+        .populate("host", ["playerInfo", "name", "_id"])
+        .populate("game");
+      const user = await User.findById(req.user.id)
+        .select("-email")
+        .select("-password");
+
+      const alreadyInLobby = lobby.players.filter(
+        player => player._id.toString() === user.id.toString()
+      );
+      // If user already exists in lobby, update the user to avoid duplicates
+      if (alreadyInLobby.length) {
+        lobby.players.map(player => {
+          if (player._id.toString() === user.id.toString()) {
+            player = user;
+          }
+        });
+      } else {
+        lobby.players.push(user);
+      }
+      await lobby.save();
+      res.json(lobby);
+    } catch (error) {
+      res.status(300).json(error);
+    }
+  },
+  leaveLobby: async (req, res) => {
+    try {
+      const lobby = await Lobby.findById(req.params.lobby_id);
+      const user = await User.findById(req.user.id)
+        .select("-email")
+        .select("-password");
+
+      const inLobby = lobby.players.filter(
+        player => player._id.toString() === user.id.toString()
+      );
+      if (!inLobby.length) {
+        return res
+          .status(404)
+          .json({ msg: "You have not yet joined this lobby" });
+      }
+      const players = lobby.players.filter(
+        player => player._id.toString() !== user.id.toString()
+      );
+
+      lobby.players = players;
+      await lobby.save();
+      res.json(user);
+    } catch (error) {
+      res.status(300).json(error);
+    }
+  },
+  deleteLobby: async (req, res) => {
+    try {
+      const lobby = await Lobby.findById(req.params.lobby_id);
+      const lobbies = await Lobby.find();
+
+      const user = await User.findById(req.user.id)
+        .select("-password")
+        .select("-email");
+
+      if (!lobby) {
+        return res.status(401).json({ msg: "Lobby not found" });
+      }
+
+      if (lobby.host._id.toString() === user.id) {
+        await lobby.remove();
+        return res.json(lobbies);
+      } else {
+        return res.json({
+          msg: "Can't delete the lobby if you are not the host"
+        });
+      }
+    } catch (error) {
+      res.status(300).json(error);
+    }
+  },
+  deleteAllLobbies: async (req, res) => {
+    try {
+      await Lobby.deleteMany({}, err => {
+        if (err) throw err;
+      });
+
+      return res.status(200).json({ msg: "All lobbies have been deleted" });
+    } catch (error) {
+      res.status(300).json(error);
+    }
+  }
+};
--- routes/api/lobby.js
+++ routes/api/lobby.js
@@ -1,205 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const Lobby = require("../../models/Lobby");
-const User = require("../../models/User");
-const Game = require("../../models/Game");
+const lobbyController = require("../../controllers/lobbyController");
+const auth = require("../../middleware/auth");
 
 router.get("/test", (req, res) => res.send("Lobbys works"));
 
 // Create new lobby
-router.post("/:user_id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.user_id)
-      .select("-password")
-      .select("-email");
-    if (!user) {
-      return res
-        .status(404)
-        .json({ msg: "Can't create lobby under a user that does not exist." });
-    }
-
-    const { name } = req.body;
-    let lobby = await Lobby.findOne({ name });
-
-    if (lobby) {
-      return res
-        .status(400)
-        .json({ msg: "Lobby with that name already exists" });
-    }
-
-    const gameSetup = {
-      start: false,
-      gif: null,
-      players: [],
-      whiteCardPlayers: [],
-      blackCardPlayer: null,
-      whiteCardPicks: [],
-      blackCardPick: null,
-      gameWinner: null
-    };
-    const game = new Game(gameSetup);
-
-    await game.save();
-
-    let lobbySetup = {
-      name,
-      host: user,
-      players: [user],
-      game
-    };
-    lobby = new Lobby(lobbySetup);
-    await lobby.save();
-    res.status(200).json(lobby);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
-});
+router.post("/create/:user_id", auth, lobbyController.createLobby);
 
 // Get all lobbies
-router.get("/", async (req, res) => {
-  try {
-    let lobbies = await Lobby.find()
-      .populate("host", ["name", "_id"])
-      .populate("game")
-      .populate("players");
-
-    if (!lobbies.length) {
-      return res.status(404).json({ msg: "No lobbies have been created." });
-    }
-
-    res.status(200).json(lobbies);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error.");
-  }
-});
+router.get("/", lobbyController.getAllLobbies);
 
 // Get Lobby by ID
-router.get("/:lobby_id", async (req, res) => {
-  try {
-    const lobby = await Lobby.findById(req.params.lobby_id)
-      .populate("host")
-      .populate("game")
-      .populate("players");
-
-    if (!lobby) {
-      return res.status(404).json({ msg: "Lobby not found." });
-    }
-
-    res.status(200).json(lobby);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error.");
-  }
-});
-
-// Delete Lobby by ID
-router.delete("/:lobby_id", async (req, res) => {
-  try {
-    let lobby = await Lobby.findById(req.params.lobby_id);
-    if (!lobby) {
-      return res.status(404).json({ msg: "Lobby not found" });
-    }
-
-    await Lobby.findOneAndRemove({ _id: lobby._id });
-    let lobbies = await Lobby.find();
-    res.status(200).json({ msg: "Lobby Deleted", lobbies });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error.");
-  }
-});
+router.get("/:lobby_id", lobbyController.getLobbyByID);
 
 // Players Join Lobby
-router.put("/:lobby_id/:player_id", async (req, res) => {
-  try {
-    let lobby = await Lobby.findById(req.params.lobby_id)
-      .populate("host")
-      .populate("game")
-      .populate("players");
-
-    if (!lobby) {
-      return res.status(404).json({ msg: "Lobby doesn't exist." });
-    }
-
-    let player = await User.findById(req.params.player_id)
-      .select("-password")
-      .select("-email");
-    if (!player) {
-      return res.status(404).json({ msg: "Player doesn't exist" });
-    }
-
-    let alreadyInLobby = lobby.players.filter(
-      person => person._id.toString() === player._id.toString()
-    );
-
-    if (alreadyInLobby.length) {
-      return res.status(400).json({ msg: "You are already in this lobby." });
-    }
-
-    lobby.players.push(player);
-    await lobby.save();
-    res.status(200).json(lobby);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-});
+router.post("/join/:lobby_id", auth, lobbyController.joinLobby);
 
 // Player leave lobby
-router.delete("/:lobby_id/:player_id", async (req, res) => {
-  try {
-    let lobby = await Lobby.findById(req.params.lobby_id)
-      .populate("host")
-      .populate("game")
-      .populate("players");
+router.delete("/leave/:lobby_id", auth, lobbyController.leaveLobby);
 
-    if (!lobby) {
-      return res.status(404).json({ msg: "Lobby doesn't exist" });
-    }
-
-    let player = await User.findById(req.params.player_id).select("-password");
-    if (!player) {
-      return res.status(404).json({ msg: "Player not found" });
-    }
-
-    // Check to see if player exists in lobby
-    let alreadyInLobby = lobby.players.filter(
-      person => person._id.toString() === player._id.toString()
-    );
-
-    if (!alreadyInLobby.length) {
-      return res
-        .status(400)
-        .json({ msg: "You can't leave a lobby that you do not exist in." });
-    }
-
-    let updatedLobby = lobby.players.filter(
-      person => person._id.toString() !== player._id.toString()
-    );
-
-    lobby.players = updatedLobby;
-
-    await lobby.save();
-    res.status(200).json(lobby);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-});
-
+// Delete Lobby by ID
+router.delete("/delete/:lobby_id", auth, lobbyController.deleteLobby);
 // Delete all lobbies
-router.delete("/", async (req, res) => {
-  try {
-    const lobbies = await Lobby.deleteMany({}, err => {
-      if (err) throw err;
-    });
-
-    res.status(200).json(lobbies);
-  } catch (error) {
-    console.error(error);
-  }
-});
+router.delete("/deleteall", lobbyController.deleteAllLobbies);
 
 module.exports = router;
